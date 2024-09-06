20929738244baa2ac654df8b694d5525eae64215
Minaana <emmanuelnanaopokumanu@outlook.com>
2024-09-05 21:22:37 -0400

    Implementing Custom Permissions in Django

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -66,55 +66,10 @@ def member_view(request):
     return render(request, 'relationship_app/member_view.html')
 
 
-
-# relationship_app/views.py
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm  # Assuming you have a form for Book
-
-# Add a new book (only users with 'can_add_book' permission)
-@permission_required('relationship_app.can_add_book')
-def add_book(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('book_list')  # Redirect to a list of books or another appropriate view
-    else:
-        form = BookForm()
-    return render(request, 'relationship_app/add_book.html', {'form': form})
-
-# Edit an existing book (only users with 'can_change_book' permission)
-@permission_required('relationship_app.can_change_book')
-def edit_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
-        if form.is_valid():
-            form.save()
-            return redirect('book_list')
-    else:
-        form = BookForm(instance=book)
-    return render(request, 'relationship_app/edit_book.html', {'form': form})
-
-# Delete a book (only users with 'can_delete_book' permission)
-@permission_required('relationship_app.can_delete_book')
-def delete_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        book.delete()
-        return redirect('book_list')
-    return render(request, 'relationship_app/delete_book.html', {'book': book})
-
-
-
-
-# relationship_app/views.py
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import permission_required
-from .models import Book
-from .forms import BookForm  # Assuming you have a form for Book
+from .forms import BookForm  # Ensure you have this form
 
 @permission_required('relationship_app.can_add_book')
 def add_book(request):
@@ -122,7 +77,7 @@ def add_book(request):
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('book_list')  # Redirect to a list of books or another appropriate view
+            return redirect('book_list')  # Ensure 'book_list' is defined
     else:
         form = BookForm()
     return render(request, 'relationship_app/add_book.html', {'form': form})
