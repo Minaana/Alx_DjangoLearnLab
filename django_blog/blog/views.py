f7764fad534324a96dabbaa403a60c133ba54e99
Minaana <emmanuelnanaopokumanu@outlook.com>
2024-09-26 22:59:24 -0400

    2. Creating Blog Post Management Features

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -24,26 +24,10 @@ def profile_view(request):
     return render(request, 'registration/profile.html', {'form': form})
 
 
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from django.views.generic import UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post
 
-class PostListView(ListView):
-    model = Post
-    template_name = 'blog/post_list.html'  # <app>/<model>_<viewtype>.html
-    context_object_name = 'posts'
-    ordering = ['-date_posted']  # Show latest posts first
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'blog/post_detail.html'
-class PostCreateView(LoginRequiredMixin, CreateView):
-    model = Post
-    fields = ['title', 'content']
-    template_name = 'blog/post_form.html'
-
-def form_valid(self, form):
-    form.instance.author = self.request.user
-    return super().form_valid(form)
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
@@ -56,11 +40,4 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Post
-    success_url = '/'
-    template_name = 'blog/post_confirm_delete.html'
 
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
